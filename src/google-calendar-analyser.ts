dc565e1a8d18ee01707056f662672217deeabffa
Simon Coope <simon.coope@sjcnet.co.uk>
2020-05-28 12:52:39 +1000

    Added basic Clasp project set-up

---
+++ src/google-calendar-analyser.ts
@@ -0,0 +1,22 @@
+function onOpen(e) {
+  let ui = SpreadsheetApp.getUi();
+  let menu = ui.createAddonMenu();
+  menu.addItem('Create Settings Sheet', 'createSettingsSheet');
+  menu.addItem('Help', 'showHelpDialog');
+  menu.addToUi();
+
+  Logger.log('Started aws-pricing');
+}
+
+function onInstall(e) {
+  onOpen(e);
+}
+
+function showHelpDialog() {
+  let ui = SpreadsheetApp.getUi();
+  let html = HtmlService.createHtmlOutputFromFile('files/help_dialog');
+  html.setTitle('Google Calendar Analyser - Help');
+  ui.showSidebar(html);
+}
+
+function createSettingsSheet() {}
