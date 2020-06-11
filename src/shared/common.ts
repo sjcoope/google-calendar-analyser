e742353a6a85425be2235fb659e1fda15353b55b
Simon Coope <simon.coope@sjcnet.co.uk>
2020-06-11 20:38:18 +1000

    Added common class and tests

--- src/shared/common.ts
+++ src/shared/common.ts
@@ -7,3 +7,18 @@ export class KeyValuePair {
   key: string;
   value: string;
 }
+
+export class Convertor {
+  static toMultiDimensionalArray(items: Array<KeyValuePair>): Array<Array<String>> {
+    if (!items) {
+      throw new Error('toMultiDimensionalArray: Invalid parameter');
+    }
+
+    var result = new Array();
+    for (var i = 0; i < items.length; i++) {
+      var item = items[i];
+      result.push(new Array(item.key, item.value));
+    }
+    return result;
+  }
+}
---
+++ test/shared/common.test.ts
@@ -0,0 +1,32 @@
+import { Convertor, KeyValuePair } from '../../src/shared/common';
+import { expect } from 'chai';
+
+describe('Common', () => {
+  describe('Convertor', () => {
+    describe('toMultiDimensionalArray', () => {
+      it('should throw error if input is invalid', () => {
+        expect(Convertor.toMultiDimensionalArray.bind(null)).to.throw(Error);
+        //expect(dateHelper.formatDate.bind(null)).to.throw('formatDate - date parameter is invalid');
+        //expect(Convertor.toMultiDimensionalArray(null)).to.throw(Error);
+      });
+
+      it('should return array with length 1 when presented with array length 1', () => {
+        var input = new Array<KeyValuePair>(new KeyValuePair('1', 'One'));
+        var result = Convertor.toMultiDimensionalArray(input);
+        expect(result.length).to.equal(1);
+      });
+
+      it('should return array with item that has matching key', () => {
+        var input = new Array<KeyValuePair>(new KeyValuePair('1', 'One'));
+        var result = Convertor.toMultiDimensionalArray(input);
+        expect(result[0][0]).to.equal(input[0].key);
+      });
+
+      it('should return array with item that has matching value', () => {
+        var input = new Array<KeyValuePair>(new KeyValuePair('1', 'One'));
+        var result = Convertor.toMultiDimensionalArray(input);
+        expect(result[0][1]).to.equal(input[0].value);
+      });
+    });
+  });
+});
